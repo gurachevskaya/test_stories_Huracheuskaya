234cf41378c1f24b77491662afec0b07debdca9a
Karyna <karyna.huracheuskaya@accenture.com>
2025-05-15 14:32:23 +0200

    Project init

---
+++ test_Stories/test_StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_StoriesApp.swift
+//  test_Stories
+//
+//  Created by karyna.huracheuskaya on 15/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct test_StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test_StoriesTests/test_StoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_StoriesTests.swift
+//  test_StoriesTests
+//
+//  Created by karyna.huracheuskaya on 15/05/2025.
+//
+
+import Testing
+@testable import test_Stories
+
+struct test_StoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
